61c0aa2f59eed064311420877801f29922b5f9bc
Ankit <ankitchauhan21500@gmail.com>
2024-04-20 20:24:57 +0530

    added search bar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import "./App.css"
-import { combineReducers } from 'redux';
 
 export default function App() {
   const [chartUrl, setChartUrl] = useState('');
-  const [submission, setSubmission] = useState([]);
-  const [rating, setRating] = useState(null); // Initialize as null
-
+  const [submission, setSubmission] = useState(null);
+  const [rating, setRating] = useState([]);
+  const [username, setUsername] = useState('ankitchauhan21');
+  const [name, setname] = useState(null);
+  const [avatar, setAvatar] = useState(null);
   useEffect(() => {
     async function fetchChart() {
       try {
-        const response = await fetch('http://127.0.0.1:5000/api/chart');
+        const response = await fetch(`http://127.0.0.1:5000/api/chart?username=${username}`);
         const blob = await response.blob();
         const url = URL.createObjectURL(blob);
         setChartUrl(url);
@@ -21,7 +22,7 @@ export default function App() {
 
     async function fetchSubmissions() {
       try {
-        const response = await fetch("http://127.0.0.1:5000/api/submissions");
+        const response = await fetch(`http://127.0.0.1:5000/api/submissions?username=${username}`);
         const json = await response.json();
         setSubmission(json.recentSubmissions);
       } catch (error) {
@@ -31,7 +32,7 @@ export default function App() {
 
     async function fetchRating() {
       try {
-        const response = await fetch('http://127.0.0.1:5000/api/rating');
+        const response = await fetch(`http://127.0.0.1:5000/api/rating?username=${username}`);
         const blob = await response.blob();
         const url = URL.createObjectURL(blob);
         setRating(url);
@@ -39,7 +40,19 @@ export default function App() {
         console.error('Error fetching rating:', error);
       }
     }
+    async function getName() {
+      try {
+        const response = await fetch(`http://127.0.0.1:5000/api/name?username=${username}`);
+        const data = await response.json(); // Parse JSON response
+        console.log("data is : ", data);
+        setname(data.name); // Set the name state
+        setAvatar(data.image); // Set the name state
+      } catch (error) {
+        console.error('Error fetching name:', error);
+      }
+    }
 
+    getName();
     fetchRating();
     fetchSubmissions();
     fetchChart();
@@ -55,6 +68,47 @@ export default function App() {
     fontWeight: 'bold',
   };
 
+
+  const handleSearch = async () => {
+    setChartUrl(null);
+    setSubmission(null);
+    setRating(null);
+    setname(null);
+    setAvatar(null);
+    try {
+      const response = await fetch(`http://127.0.0.1:5000/api/chart?username=${username}`);
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+      setChartUrl(url);
+    } catch (error) {
+      console.error('Error fetching chart:', error);
+    }
+    try {
+      const response = await fetch(`http://127.0.0.1:5000/api/submissions?username=${username}`);
+      const json = await response.json();
+      setSubmission(json.recentSubmissions);
+    } catch (error) {
+      console.error('Error fetching submissions:', error);
+    }
+    try {
+      const response = await fetch(`http://127.0.0.1:5000/api/rating?username=${username}`);
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+      setRating(url);
+    } catch (error) {
+      console.error('Error fetching rating:');
+    }
+    try {
+      const response = await fetch(`http://127.0.0.1:5000/api/name?username=${username}`);
+      const data = await response.json(); // Parse JSON response
+      console.log("data is : ", data);
+      setname(data.name); // Set the name state
+      setAvatar(data.image); // Set the name state
+    } catch (error) {
+      console.error('Error fetching name:', error);
+    }
+  };
+
   return (
     <>
       <center>
@@ -62,12 +116,37 @@ export default function App() {
       </center>
       <br />
       <br />
+      <center>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Enter a valid leetcode user name' style={{ padding: "10px", fontSize: "15px" }} />
+        <br /><br />
+        <button onClick={handleSearch}>Search</button>
+      </center>
+      <br />
+      <br />
+      <center>
+        <h3>
+          {
+            avatar == null ? <div style={{ fontSize: "10px" }} className="loader2"></div>
+              :
+              <img style={{ borderRadius: "30px", height: "50px", width: "50px" }} src={avatar} alt="" />
+          }
+        </h3>
+        {
+          name == null ?
+            <div style={{ fontSize: "10px" }} className='loader2'></div>
+            :
+            <h2>
+              {name}
+            </h2>
+        }
+      </center>
+      <br />
       <section className='graphSection'>
         <div>
-          {!chartUrl ? <div className="loader2"></div> : <img src={chartUrl} alt="Chart" />}
+          {!chartUrl ? <div className="loader"></div> : <img src={chartUrl} alt="Chart" />}
         </div>
         <div>
-          {rating === null ? <div className="loader2"></div> : <img src={rating} alt="ratingGraph" />}
+          {rating === null ? <div className="loader"></div> : <img src={rating} alt="ratingGraph" />}
         </div>
       </section>
       <br />
@@ -80,7 +159,7 @@ export default function App() {
           <div style={{ fontWeight: "bolder" }}>Status</div>
           <div style={{ fontWeight: "bolder" }}>Language</div>
         </section>
-        {submission.length === 0 ? (
+        {submission === null ? (
           <div className='loader'></div>
         ) : (
           submission.map((elem, index) => {
