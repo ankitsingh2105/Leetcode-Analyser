9d2a212866e0f7a1eaf79e212ab20f8ec4fbe276
Ankit <ankitchauhan21500@gmail.com>
2024-04-23 10:25:25 +0530

    added new files

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 import matplotlib
-matplotlib.use('Agg')  # Use the Agg backend
+matplotlib.use('Agg')  # Use the Agg backend for generating hight quality graphics
 
 import io
 import matplotlib.pyplot as plt
-from flask import Flask, send_file, jsonify
+from flask import Flask, send_file, jsonify, request
 from flask_cors import CORS
 import requests
+import mysql.connector as connector
+
+connect = connector.connect(
+    host="localhost",
+    user="root",
+    password="thechauhan1",
+    database="Analyser"
+)
+
+cursor = connect.cursor();
 
 app = Flask(__name__)
 CORS(app)
+# CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
+@app.after_request
+def add_cors_headers(response):
+    # Allow requests from all origins
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 @app.route('/')
 def hello_world():
@@ -18,7 +36,17 @@ def hello_world():
 def generate_chart():
     try:
         # Make a request to fetch data
-        response = requests.get('https://leetcode-api-faisalshohag.vercel.app/ankitchauhan21')
+        username = request.args.get('username')
+        # cursor.execute("INSERT INTO NAME (names) values (%s) ", username)
+        # connect.commit();
+        with open('usernames.txt', 'a') as file:
+            file.write(username + '\n')
+
+        if not username:
+            return jsonify({'error': 'Username is required'})
+
+        # Make a request to fetch data for the specified username
+        response = requests.get(f'https://leetcode-api-faisalshohag.vercel.app/{username}')
         data = response.json()
         
         # Extract x and y values from the data
@@ -63,40 +91,74 @@ def generate_chart():
 @app.route("/api/rating")
 def get_rating():
     try:
-        # Sample data
-        data = [1507,1501, 1489, 1522,1541, 1562, 1585, 1564, 1571, 1573, 1582,  1646, 1630, 1687, 1705, 1720, 1719]
+        # Make a request to fetch data
+        username = request.args.get('username') 
+        if not username:
+            return jsonify({'error': 'Username is required'})
+
+        # Make a request to fetch data for the specified username
+        response = requests.get(f'https://alfa-leetcode-api.onrender.com/{username}/contest')
+        data = response.json()['contestParticipation']
+        print(data)
+        # Extract contest ratings
+        ratings = [entry['rating'] for entry in data]
 
         # Create a line chart using Agg backend
         plt.switch_backend('Agg')
-        plt.plot(range(1, len(data) + 1), data, marker='o', color='gold', linestyle='-')  # Customize line color, marker, and linestyle
-        plt.xlabel('contest', fontsize=12, fontweight='bold')  # Set x-axis label with bold font
-        plt.ylabel('Rating', fontsize=12, fontweight='bold')  # Set y-axis label with bold font
-        plt.title('Rating Over Time', fontsize=14, fontweight='bold')  # Set chart title with bold font
+        plt.plot(range(1, len(ratings) + 1), ratings, marker='o', color='gold', linestyle='-')  
+        plt.xlabel('Contest', fontsize=12, fontweight='bold')  
+        plt.ylabel('Rating', fontsize=12, fontweight='bold')  
+        plt.title('Rating Over Time', fontsize=14, fontweight='bold')  
 
-        # setting up the x and y axis name with size
-        plt.xticks(range(1, len(data) + 1), fontsize=10)
+        # Customize the appearance
+        plt.xticks(fontsize=10)
         plt.yticks(fontsize=10)
-        plt.grid(axis='y', linestyle='-', alpha=0.7)  # Add grid lines only on y-axis
-        plt.grid(axis='x', linestyle='-', alpha=0.7)  # Add grid lines only on y-axis
+        plt.grid(axis='y', linestyle='-', alpha=0.7)
+        plt.grid(axis='x', linestyle='-', alpha=0.7)
 
         # Save the chart as BytesIO object
         img_bytes = io.BytesIO()
-        plt.savefig(img_bytes, format='png', bbox_inches='tight', pad_inches=0.1)  # Adjust padding and tight layout
+        plt.savefig(img_bytes, format='png', bbox_inches='tight', pad_inches=0.1)
         img_bytes.seek(0)
         plt.close()
 
         # Return the image file
         return send_file(img_bytes, mimetype='image/png')
     except Exception as e:
-        return str(e)
+        return jsonify({'error': str(e)})
+    
+
+@app.route("/api/name")
+def get_name():
+    try:
+        username = request.args.get('username') 
+
+        # Make a request to fetch data for the specified username
+        response = requests.get(f'https://alfa-leetcode-api.onrender.com/{username}')
+
+        data = response.json()
+        
+        # Check if the response contains the expected data structure
+        name = data['name']
+        image = data['avatar']
+        return jsonify({'name': name, 'image': image})
+    except Exception as e:
+        print("Error:", e)
+        return jsonify({'error': 'An error occurred'})
+
+
 
 @app.route('/api/submissions')
 def get_leetcode_data():
     try:
         # Make a GET request to the LeetCode API
-        response = requests.get('https://leetcode-api-faisalshohag.vercel.app/ankitchauhan21')
-        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
-        data = response.json()  # Convert response to JSON format
+        username = request.args.get('username') 
+        if not username:
+            return jsonify({'error': 'Username is required'})
+
+        # Make a request to fetch data for the specified username
+        response = requests.get(f'https://leetcode-api-faisalshohag.vercel.app/{username}')
+        data = response.json()
         return jsonify(data)  # Return the JSON data from the LeetCode API
     except requests.exceptions.RequestException as e:
         return jsonify({'error': str(e)}) 
